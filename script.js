51b07ddf3bc8559192d1a594b3b8e071fe83ef31
Isaac Arnold <isaacarnold@icloud.com>
2021-01-18 19:53:04 +1300

    Adding functionality to the dropdown chevrons

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 /* ====================================
 Global Variables
 =====================================*/
-const chevron = document.querySelectorAll("#chevron");
+// const chevron = document.querySelectorAll("#chevron");
+const answerP = document.querySelectorAll(".answer");
+const questionWrapper = document.querySelectorAll(".question-wrapper");
 
-chevron.forEach((arrow) =>
-  arrow.addEventListener("click", (e) => {
-    if (e.target === arrow) {
-      console.log("Success");
-    }
-  })
-);
+console.log(questionWrapper);
+
+questionWrapper.forEach((question) => {
+  let chevron = question.querySelector("#chevron");
+  let answer = question.querySelector(".answer");
+  let questionDiv = question.querySelector(".question");
+
+  questionDiv.addEventListener("click", () => {
+    answer.classList.toggle("hide");
+    questionDiv.querySelector(".question p").classList.toggle("bold");
+  });
+});
